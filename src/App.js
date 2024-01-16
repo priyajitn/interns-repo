d1ce1b76d9b15e23d6581deceec87b050f4e1392
Anurag Mishra <anurag.mishra@eximietas.design>
2024-01-16 10:59:40 +0530

    feat(ui): Implement Todo List using Redux store and slice reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './redux/todoSlice';
+import Todos from './components/Todos';
+import './App.css'
+const store = configureStore({
+  reducer: {
+    todos: todoReducer,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Todos />
+      </div>
+    </Provider>
   );
 }
 
-export default App;
+export default App;
